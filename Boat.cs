067d81cee3707536c5c13bd231c160b22b604816
bstal254@gmail.com <jgPgk7pi>
2017-12-25 15:31:15 +0200

    Initial Commit

---
+++ Boat.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMillionersIssues
+{
+    class Boat
+    {
+        private static readonly int numOfSailorsPerBoat = 10;
+
+        private static IPAddress IPAdd = initIPAddress();
+        private static int currentSocket = 11000;
+        private static List<Socket> listenList = new List<Socket>();
+        private static List<Socket> acceptList = new List<Socket>();
+
+        private char[] boatName = new char[32];
+        private Socket socket;
+        private Boolean anchor;
+
+        private static IPAddress initIPAddress()
+        {
+            IPHostEntry ipHostEntry = Dns.Resolve(Dns.GetHostName());
+            return ipHostEntry.AddressList[0];
+        }
+
+
+        public Boat(String name)
+        {
+            int i;
+            for (i = 0; i < name.Length; i += 1)
+                this.boatName[i] = name[i];
+            while (i < boatName.Length)
+            {
+                this.boatName[i] = ' ';
+                i += 1;
+            }
+
+
+            this.socket = new Socket(AddressFamily.InterNetwork,
+                               SocketType.Stream, ProtocolType.Tcp);
+            this.socket.Bind(new IPEndPoint(IPAdd, currentSocket));
+            this.socket.Listen(numOfSailorsPerBoat);
+            this.anchor = false;
+
+
+            listenList.Add(socket);
+
+        }
+
+        public void raiseTheSail()
+        {
+            new System.Threading.Timer((e) =>
+            {
+                broadcast();
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(60));
+
+            while (!anchor)
+            {
+                
+            }
+
+           
+
+          
+
+        }
+        
+        private void broadcast()
+        {
+            int i, j;
+            String boatName = new string(this.boatName);       
+            byte[] info = Encoding.ASCII.GetBytes("IntroToNets" + boatName);
+            byte[] port = BitConverter.GetBytes(Int16.Parse(((IPEndPoint)socket.LocalEndPoint).Port.ToString()));
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(port);
+   
+            byte[] application = new byte[info.Length + port.Length];
+            
+            for (i = 0; i < info.Length; i += 1)
+                application[i] = info[i];
+            for (j = 0; j < port.Length; j += 1, i += 1)
+                application[i] = port[j];
+
+
+            new UdpClient().Send(application, application.Length , new IPEndPoint(IPAddress.Broadcast, 5656));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Millioners.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TheMillionersIssues
+{
+    class Milioner
+    {
+        private string milionerName;
+        private UdpClient sailorPort;
+
+        public Milioner(String name)
+        {
+            this.milionerName = name;
+
+        }
+
+        public void lookingForSomeTrip()
+        {
+            this.sailorPort = new UdpClient(6565);
+            Console.Write(this.sailorPort.Client);
+            Console.WriteLine("Looking for a new boat...");
+            this.sailorPort.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
+            this.sailorPort.Client.Listen(10);
+            this.sailorPort.BeginReceive(Read_Callback, new object());
+        }
+
+        public class StateObject
+        {
+            public Socket workSocket = null;
+            public const int BUFFER_SIZE = 1024;
+            public byte[] buffer = new byte[BUFFER_SIZE];
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        public static void Read_Callback(IAsyncResult ar)
+        {
+            StateObject so = (StateObject)ar.AsyncState;
+            Socket s = so.workSocket;
+
+            int read = s.EndReceive(ar);
+
+            if (read > 0)
+            {
+                so.sb.Append(Encoding.ASCII.GetString(so.buffer, 0, read));
+                s.BeginReceive(so.buffer, 0, StateObject.BUFFER_SIZE, 0,
+                                         new AsyncCallback(Read_Callback), so);
+            }
+            else
+            {
+                if (so.sb.Length > 1)
+                {
+                    //All of the data has been read, so displays it to the console
+                    string strContent;
+                    strContent = so.sb.ToString();
+                    Console.WriteLine(String.Format("Read {0} byte from socket" +
+                                       "data = {1} ", strContent.Length, strContent));
+                }
+                s.Close();
+            }
+        }
+    }
+
+}
