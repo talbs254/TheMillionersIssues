96e6b5640ff92a6011d695eae43e326a4e82e847
amitmag <amitmag@post.bgu.ac.il>
2017-12-27 16:26:14 +0200

    new code

--- Boat.cs
+++ Boat.cs
@@ -10,41 +10,40 @@ namespace TheMillionersIssues
 
     class Boat
     {
-        private static readonly int numOfSailorsPerBoat = 10;
 
-        private static List<Socket> listenList = new List<Socket>();
-        private static List<Socket> acceptList = new List<Socket>();
-
-        private static int boatSocketCtr = 10000;
-
-        private char[] boatName = new char[32];
-        private Socket socket;
+        private List<Socket> millionerstList;
+        private Dictionary<String, Tuple<int, Socket>> millionersDictionary;
+        private TcpListener server;
+        private String boatName;
+        private int port;
         private Boolean anchor;
 
-       
 
-
+     
         public Boat(String name)
         {
-            int i;
-            for (i = 0; i < name.Length; i += 1)
-                this.boatName[i] = name[i];
-            while (i < boatName.Length)
-            {
-                this.boatName[i] = ' ';
-                i += 1;
-            }
-
-
-            this.socket = new Socket(AddressFamily.InterNetwork,
-                               SocketType.Stream, ProtocolType.Tcp);
-            this.socket.Bind(new IPEndPoint(IPAddress.Any, boatSocketCtr));
-            boatSocketCtr += 1;
-            this.socket.Listen(numOfSailorsPerBoat);
+         
+            this.millionersDictionary = new Dictionary<string, Tuple<int, Socket>>();
+            this.millionerstList = new List<Socket>();
             this.anchor = false;
+            this.boatName = name;
+            this.port = findFreeTcpPort();
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
 
+            //initial boat name
+            char[] boatNameArray = new char[32];
+             int i;
+              for (i = 0; i < name.Length; i += 1)
+                boatNameArray[i] = name[i];
+              while (i < boatName.Length)
+              {
+                boatNameArray[i] = ' ';
+                  i += 1;
+              }
 
-            listenList.Add(socket);
+            this.boatName = new string(boatNameArray);
+              
 
         }
 
@@ -55,44 +54,68 @@ namespace TheMillionersIssues
                 broadcast();
             }, null, TimeSpan.Zero, TimeSpan.FromSeconds(60));
 
+
             while (!anchor)
             {
-                
-            }
-
-           
-
-          
+                /*
+                Socket socket = server.AcceptSocket();
+                if (socket.Connected)
+                {
+                    millionerstList.Add(socket);
+                    millionerJoindToBoat();
+                }
+                */
+             }
+         
 
         }
         
         public void broadcast()
         {
-            int i, j;
-            String boatName = new string(this.boatName);       
-            byte[] info = Encoding.ASCII.GetBytes("IntroToNets" + boatName);
-            byte[] port = BitConverter.GetBytes(Int16.Parse(((IPEndPoint)socket.LocalEndPoint).Port.ToString()));
-            if (BitConverter.IsLittleEndian)
-                Array.Reverse(port);
-   
-            byte[] application = new byte[info.Length + port.Length];
+                    
+           // String convertedPort = Convert.ToString(port, 2);
+
+            byte[] convertedPort = BitConverter.GetBytes(Int16.Parse(port.ToString()));
+             if (BitConverter.IsLittleEndian)
+                 Array.Reverse(convertedPort);
+            String applicationMessege = "IntroToNets" + boatName;
+            Byte[] info = Encoding.ASCII.GetBytes(applicationMessege);
+
+
             
+            byte[] application = new byte[info.Length + convertedPort.Length];
+            int i, j;
             for (i = 0; i < info.Length; i += 1)
                 application[i] = info[i];
-            for (j = 0; j < port.Length; j += 1, i += 1)
-                application[i] = port[j];
+            for (j = 0; j < convertedPort.Length; j += 1, i += 1)
+                application[i] = convertedPort[j];
 
-
+    
             new UdpClient().Send(application, application.Length , new IPEndPoint(IPAddress.Broadcast, 5656));
 
-
-
-
-
-
-
-
-
         }
+        /// <summary>
+        /// find available port in system
+        /// </summary>
+        /// <returns> available port </returns>
+        private static int findFreeTcpPort()
+        {
+            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
+
+        private void millionerJoindToBoat()
+        {
+            foreach(Socket socket in millionerstList)
+            {
+                String messege = "Welcome to " + boatName + "! What is your name?";
+                socket.Send(Encoding.ASCII.GetBytes(messege));
+            }
+        }
+
+        
     }
 }
--- Millioners.cs
+++ Millioners.cs
@@ -13,24 +13,29 @@ namespace TheMillionersIssues
         public Milioner(String name)
         {
             this.milionerName = name;
+            
 
         }
 
         public void lookingForSomeTrip()
         {
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 5656);
-            this.sailorPort = new UdpClient(RemoteIpEndPoint);
+           
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 5656);
+            sailorPort = new UdpClient(ipEndPoint);
             Console.WriteLine("Looking for a new boat...");
-         
-            // Blocks until a message returns on this socket from a remote host.
-            Byte[] receiveBytes = this.sailorPort.Receive(ref RemoteIpEndPoint);
-            Byte[] portBytes = { receiveBytes[receiveBytes.Length - 1], receiveBytes[receiveBytes.Length - 2] };
-
-            
+            Byte[] receiveBytes = sailorPort.Receive(ref ipEndPoint);
+            Byte[] portBytes = { receiveBytes[receiveBytes.Length - 1], receiveBytes[receiveBytes.Length - 2] };           
             string applicationMsg = Encoding.ASCII.GetString(receiveBytes);      
             int returnPort = BitConverter.ToInt16(portBytes, 0);
 
             Console.WriteLine("Requesting to board The " + applicationMsg.Substring("IntroToNets".Length, 32/*bytes*/));
+            TcpClient tcpConnection = new TcpClient("server", returnPort); // add port
+
+            if (tcpConnection.Connected)
+            {
+
+            }
+
 
             
           
